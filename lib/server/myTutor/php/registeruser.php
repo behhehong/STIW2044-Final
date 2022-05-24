8b2a8cc6e89b1decca7f9dc6cdb0c1e33363cef2
behhehong <hehongbeh@gmail.com>
2022-05-24 08:35:42 +0800

    version 0.1

--- lib/server/myTutor/php/registeruser.php
+++ lib/server/myTutor/php/registeruser.php
@@ -5,10 +5,9 @@ $phone_Num=$_POST['phoneNum'];
 $address=$_POST['address'];
 $email=$_POST['email'];
 $password=$_POST['password'];
-$userImage=$_POST['userImage'];
 $passsha1=sha1($password);
 
-$sqlregister="INSERT INTO tbl_users(username,phone_Num,address,email,password,userImage) VALUES('$username','$phone_Num','$address','$email','$passsha1','$userImage')";
+$sqlregister="INSERT INTO tbl_users(username,phone_Num,address,email,password) VALUES('$username','$phone_Num','$address','$email','$passsha1')";
 
 if ($conn->query($sqlregister) === TRUE){
     echo "Success";
